af5dc88d2f7988cd0fcd28e10f310c6e9c6b0b47
Brendan Bone <elmerohueso@gmail.com>
2015-06-30 19:48:13 -0600

    Avoid junk downloads
    Added option to wait until the day after an episode airs to download it
    in an attempt to avoid junk/spam/fake downloads

--- sickbeard/tv.py
+++ sickbeard/tv.py
@@ -851,15 +851,22 @@ class TVShow(object):
             return False
 
         myDB = db.DBConnection()
-        sqlResults = myDB.select("SELECT status FROM tv_episodes WHERE showid = ? AND season = ? AND episode = ?", [self.tvdbid, season, episode])
+        sqlResults = myDB.select("SELECT status,airdate FROM tv_episodes WHERE showid = ? AND season = ? AND episode = ?", [self.tvdbid, season, episode])
 
         if not sqlResults or not len(sqlResults):
             logger.log(u"Unable to find the episode", logger.DEBUG)
             return False
 
         epStatus = int(sqlResults[0]["status"])
-
         logger.log(u"current episode status: " + str(epStatus), logger.DEBUG)
+		
+        epAirdate = datetime.date.fromordinal(sqlResults[0]["airdate"])
+        logger.log(u"current episode air date: " + str(epAirdate), logger.DEBUG)
+		
+        # if episode hasn't aired yet, say no to avoid junk downloads
+        if epAirdate >= (datetime.date.today()) and sickbeard.DOWNLOAD_AVOID_JUNK:
+            logger.log(u"Ep hasn't aired yet, skipping to avoid junk downloads", logger.DEBUG)
+            return False
 
         # if we know we don't want it then just say no
         if epStatus in (SKIPPED, IGNORED, ARCHIVED) and not manualSearch:
