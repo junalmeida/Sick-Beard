junalmeida/Sick-Beard

commit 371d37928469e3615a12c84ce0258106bdf55a17

Add default address for browser launch to 127.0.0.1 as windows does not like 0.0.0.0.

SickBeard.py
@@ -358,7 +358,7 @@ def main():
         if sickbeard.WEB_IPV6:
             webhost = '::'
         else:
-            webhost = '0.0.0.0'
+            webhost = '127.0.0.1'
 
     if forcedHost:
         logger.log(u"Forcing web server to address " + str(forcedHost))

sickbeard/__init__.py
@@ -476,9 +476,9 @@ def initialize(consoleLogging=True):
             WEB_PORT = 8081
 
         try:
-            WEB_HOST = check_setting_str(CFG, 'General', 'web_host', '0.0.0.0', True)
+            WEB_HOST = check_setting_str(CFG, 'General', 'web_host', '127.0.0.1', True)
         except:
-            WEB_HOST = '0.0.0.0'
+            WEB_HOST = '127.0.0.1'
 
         if WEB_PORT < 21 or WEB_PORT > 65535:
             WEB_PORT = 8081

commit 30d41861e519017f4d6bd4e900f3e09c42c03fa2

Fix regression that broke IP binding in commit https://github.com/junalmeida/Sick-Beard/commit/371d37928469e3615a12c84ce0258106bdf55a17

SickBeard.py
@@ -352,13 +352,16 @@ def main():
 
     # sickbeard.WEB_HOST is available as a configuration value in various
     # places but is not configurable. It is supported here for historic reasons.
-    if sickbeard.WEB_HOST and sickbeard.WEB_HOST != '0.0.0.0':
+    if sickbeard.WEB_HOST:
         webhost = sickbeard.WEB_HOST
     else:
-        if sickbeard.WEB_IPV6:
+        webhost = '127.0.0.1'
+
+    if sickbeard.WEB_IPV6:
+        if webhost == '127.0.0.1':
+            webhost = '::1'
+        elif webhost == '0.0.0.0':
             webhost = '::'
-        else:
-            webhost = '127.0.0.1'
 
     if forcedHost:
         logger.log(u"Forcing web server to address " + str(forcedHost))